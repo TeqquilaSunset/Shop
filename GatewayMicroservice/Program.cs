77cc6442772089be1492317e3678ffcc682a9eb7
Vasiliy Bistritskiy <sc-2005@yandex.ru>
2023-11-26 23:39:39 +0700

    feat(ocelot): add delivery and order in ocelot

--- GatewayMicroservice/Program.cs
+++ GatewayMicroservice/Program.cs
@@ -1,37 +1,23 @@
+using Microsoft.Extensions.Configuration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile("ocelot.json", optional: false, reloadOnChange: false)
-                .AddEnvironmentVariables();
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: false)
+    .AddEnvironmentVariables();
 
 builder.Services.AddMvc();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 builder.Services.AddOcelot(builder.Configuration);
 
-
-//builder.Services.AddCustomJwtAuthentication();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 app.UseSwagger();
 app.UseSwaggerForOcelotUI();
 app.UseOcelot().Wait();
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-await app.UseOcelot();
-app.MapControllers();
 
 app.Run();
